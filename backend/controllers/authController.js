2aec5fd499b098deafba60b2cc746c10ada4eaca
Drashti311 <rdrashti767@gmail.com>
2025-12-27 16:57:57 +0530

    JWT token generation

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,6 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
-// Generate JWT token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '7d' });
 };
